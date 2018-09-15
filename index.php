045b8a8867476ab0e84794e233418204a51f0603
giampieer <giampieer24@gmail.com>
2018-09-15 10:32:57 -0500

    update case

--- index.php
+++ index.php
@@ -1,21 +1,30 @@
 <?php
 session_start();
 include 'simple_html_dom.php';
-$html = file_get_html('https://www.indeci.gob.pe/calendario.php');
-$tr = $html->find('li');
 
-foreach ($tr as $item) {
-  $tremor = [];
-  $a = $item->find("time", 0)->plaintext;
-  //$h2 = $item->find("h2",1)->plaintext;
-  $p = $item->find("p",0)->plaintext;
-  //$tremor['h2'] = $h2;
-  $tremor['p'] = $p;
-  if ($a != "" || $a != null) {
-  $tremor['place'] = $a;
-  $tremors[] = $tremor;
-  }
+$op = $_REQUEST['op'];
+switch ($op) {
+  case 1:{
+    $html = file_get_html('https://www.indeci.gob.pe/calendario.php');
+    $tr = $html->find('li');
+
+    foreach ($tr as $item) {
+      $tremor = [];
+      $a = $item->find("time", 0)->plaintext;
+      //$h2 = $item->find("h2",1)->plaintext;
+      $p = $item->find("p",0)->plaintext;
+      //$tremor['h2'] = $h2;
+      $tremor['p'] = $p;
+      if ($a != "" || $a != null) {
+        $tremor['place'] = $a;
+        $tremors[] = $tremor;
+      }
+    }
+    $estado['data'] = $tremors;
+    print json_encode($estado);
+  }break;
+  case 2:{
+
+  }break;
 }
-$estado['data'] = $tremors;
-print json_encode($estado);
 ?>
