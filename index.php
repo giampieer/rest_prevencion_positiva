691b948b3ded2f2503ba0f534bf3f395693f9884
giampieer <giampieer24@gmail.com>
2018-09-15 10:30:11 -0500

    update op 1

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+include 'simple_html_dom.php';
+$html = file_get_html('https://www.indeci.gob.pe/calendario.php');
+$tr = $html->find('li');
+
+foreach ($tr as $item) {
+  $tremor = [];
+  $a = $item->find("time", 0)->plaintext;
+  //$h2 = $item->find("h2",1)->plaintext;
+  $p = $item->find("p",0)->plaintext;
+  //$tremor['h2'] = $h2;
+  $tremor['p'] = $p;
+  if ($a != "" || $a != null) {
+  $tremor['place'] = $a;
+  $tremors[] = $tremor;
+  }
+}
+$estado['data'] = $tremors;
+print json_encode($estado);
+?>
